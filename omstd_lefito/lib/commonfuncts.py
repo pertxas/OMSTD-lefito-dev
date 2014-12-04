cd5efeae682fe94b0759ab75046e6f02860f2061
pertxas <pertxas@users.noreply.github.com>
2014-12-04 19:02:05 +0100

    Update commonfuncts.py

--- omstd_lefito/lib/commonfuncts.py
+++ omstd_lefito/lib/commonfuncts.py
@@ -1,1 +1,20 @@
 # -*- coding: utf-8 -*-
+
+# --------------------------------------------------------------------------
+def dorequest(url, params):
+    req = urllib.request.Request(url)
+    if params.agent is not None:
+        req.add_header("User-agent", params.agent)
+    else:
+        req.add_header("User-agent", "Soy el lefito.")
+    result = {}
+    try:
+        respuesta = urllib.request.urlopen(req)
+        result['body'] = respuesta.read()
+        result['head'] = respuesta.info()
+    except:
+        result = {
+            'body': 'error',
+            'head': 'error',
+        }
+    return result
